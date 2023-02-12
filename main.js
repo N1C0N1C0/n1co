c625aa70dba047d8b30a396608090badd9bab818
Jeongmin Kim <102003529+N1C0N1C0@users.noreply.github.com>
2023-02-12 15:08:43 +0900

    JS: Navbar menu toggle + project UI tweak

--- main.js
+++ main.js
@@ -16,15 +16,23 @@ document.addEventListener('scroll',()=>{
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click',(event)=> {
-    
     const target = event.target;
     const link = target.dataset.link;
     if(link == null) {
         return;
     }
-    console.log(event.target.dataset.link);
-    scrollIntoViews(link)
+    navbarMenu.classList.remove('open');
+    scrollIntoViews(link);
+
 });
+
+// Navbar toggle button for samll screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', ()=> {
+    navbarMenu.classList.toggle('open');
+});
+
+
 
 // Handle contact me button
 const homeMenu = document.querySelector('.home__contact');
